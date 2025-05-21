833ecf7054749a754cb5178564d90dfe56aa5941
Hafidzul Ahkam Dwika Maulana <ghafid999@gmail.com>
2025-05-21 23:56:44 +0700

    fixing admin dashboard

--- src/views/admin/dashboard/Index.jsx
+++ src/views/admin/dashboard/Index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import Shortcuts from "./Shortcut";
+import ProfileInformation from "./Information";
+import Setting from "./Setting";
+import Conversation from "./Conversation";
+import Projects from "./Project";
 
-export default function Index() {
+export default function IndexPage() {
     return (
         <>
-            <Shortcuts />
+            <div className="grid grid-cols-3 grid-rows-2 gap-4 mt-14 w-full h-screen">
+                <div className="h-fill"><Setting /></div>
+                <div className="h-fill"><ProfileInformation /></div>
+                <div className="h-fill"><Conversation /></div>
+                <div className="col-span-3"><Projects /></div>
+            </div> 
         </>
     );
 }
