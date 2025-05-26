92da345c28b26f863642bbd59e600cfc30fbd361
adzazarif <adzazarf@gmail.com>
2025-05-26 20:10:41 +0700

    add: delete token

--- src/models/BookingModel.js
+++ src/models/BookingModel.js
@@ -4,7 +4,7 @@ export class BookingModel {
     async createBooking(data){
         const res = await api.post('/bookings',data,{
             headers: {
-                'Authorization': `Bearer e17e001a-0b21-41ef-8349-b29f83a5d6f2`,
+                'Authorization': `Bearer`,
                 'Content-Type': 'multipart/form-data'
             }
         });
