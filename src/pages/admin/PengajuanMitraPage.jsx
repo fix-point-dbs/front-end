776854d7709513b86c2e656fad0be09fd1e6ba66
adzazarif <adzazarf@gmail.com>
2025-06-07 13:20:04 +0700

    fixing

--- src/pages/admin/PengajuanMitraPage.jsx
+++ src/pages/admin/PengajuanMitraPage.jsx
@@ -36,14 +36,7 @@ export function PengajuanMitraPage() {
         <div className="flex flex-1 flex-col p-3 sm:p-4 md:p-6 w-full overflow-y-auto mt-12 lg:mt-0">
             <Breadcrumbs />
             <PengajuanMitra services={services.data} isLoading={isLoading} handleAccept={handleAccept} handleReject={handleReject} />
-            <button
-            type="button"
-            className="fixed bottom-3 right-3 sm:bottom-4 sm:right-4 md:bottom-6 md:right-6 z-50 flex items-center justify-center p-3 sm:p-4 text-sm font-medium text-white bg-blue-600 rounded-full hover:bg-white hover:text-blue-600 hover:border hover:border-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-white dark:hover:text-blue-600 dark:hover:border dark:hover:border-blue-600 dark:focus:ring-blue-800"
-            onClick={handleAddMitra}
-            >
-            <FontAwesomeIcon icon={faPlus} className="w-5 h-5 sm:w-6 sm:h-6" />
-            <span className="sr-only">Add new item</span>
-            </button>
+    
         </div>
         </MotionDiv>
     </section>
