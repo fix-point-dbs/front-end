c2009b7d3981c18b71f789c331bc2b04d2929ffa
adzazarif <adzazarf@gmail.com>
2025-06-09 14:22:33 +0700

    add: consume api profile mitra

--- src/pages/mitra/SettingPage.jsx
+++ src/pages/mitra/SettingPage.jsx
@@ -1,38 +1,55 @@
 import React, { useState, useEffect } from "react";
-import Sidebar from "../../views/mitra/dashboard/Sidebar";
-import { Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import Pengaturan from "../../views/mitra/dashboard/Pengaturan";
-import { OrderPresenter } from "../../presenters/mitra/OrderPresenter";
-import Navbar from "../../views/mitra/dashboard/Navbar";
+import Sidebar from "../../views/user/dashboard/Sidebar";
+import { FaBars } from "react-icons/fa";
+import Pengaturan from "../../views/user/dashboard/Pengaturan";
+import { Routes, Route } from "react-router-dom";
 import MotionDiv from "../../utils/TransitionSmoth";
+import { ProfilePresenter } from "../../presenters/profilePresenter";
+import { getUser } from "../../utils/LocalStorage";
+import { showSuccessToast } from "../../utils/Toast";
 export function SettingPage() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const location = useLocation();
-  const [isLoading, setIsLoading] = useState(false);
-  const [orders, setOrders] = useState([]);
-  const id = 1;
 
+  const onSuccess = (message) => {
+    showSuccessToast(message);
+  }
+  const id = getUser().id;
+  const presenter = new ProfilePresenter({ onSuccess });
+  const handleUpdate = (data) => {
+    console.log(data);
+    presenter.updateUser(id, data);
+  };
 
-  const presenter = new OrderPresenter({ setOrders, setIsLoading, id });
-
-  console.log(orders);
-
+  const handleChangePassword = (data) => {
+    console.log(data);
+    presenter.changePassword(id, data);
+  };
+  
   useEffect(() => {
-    presenter.loadOrders();
   }, []);
 
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
       <div className="flex-1 w-full overflow-y-auto bg-gray-50">
-        <Navbar setSidebarOpen={setSidebarOpen} />
+        <div className="px-10 py-6 bg-white border-b shadow">
+          <div className="flex items-center gap-4">
+            <button
+              className="p-2 rounded text-biru md:hidden"
+              onClick={() => setSidebarOpen(true)}
+            >
+              <FaBars />
+            </button>
+            <h1 className="text-base font-bold sm:text-base md:text-medium lg:text-medium text-biru">
+              Halo, Etak!
+            </h1>
+          </div>
+        </div>
+
         <div className="px-10 py-6">
-          <AnimatePresence mode="wait">
-            <MotionDiv>
-              <Pengaturan />
-            </MotionDiv>
-          </AnimatePresence>
+          <MotionDiv>
+            <Pengaturan handleUpdate={handleUpdate} handleChangePassword={handleChangePassword} />
+          </MotionDiv>
         </div>
       </div>
     </div>
